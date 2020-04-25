a17fec73b325e17e3dd428f89a25962ef8353aec
alexanderWhile <alexanderwhile@gmail.com>
2020-04-25 12:21:22 -0400

    Updated after P.3

--- p1.py
+++ p1.py
@@ -13,3 +13,12 @@ bias = 3
 output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias
 
 print(output)
+
+'''
+Simplify above code using the dot product in numpy.
+
+'''
+import numpy as np 
+
+output = np.dot(weights, inputs) + bias
+print(output)
